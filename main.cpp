f4244bb936901f6fcfc0232d62a5c34f91de49b9
Matthew0826 <geisel.m@northeastern.edu>
2024-11-10 19:12:10 -0500

    Added a map

--- main.cpp
+++ main.cpp
@@ -35,35 +35,50 @@ public:
     }
 };
 
+void print_map(){
+    cout << "    |     |             |                              |        |        /       ___/" << endl;
+    cout << "    |     \\              |              ______________/         |_______/_______/ _/" << endl;
+    cout << "____|      \\             |             /                        |               _/" << endl;
+    cout << "    \\     _/            /           __/                         |         BOS o|_" << endl;
+    cout << "CHI o\\___/____________/       _____/  |__________________       |_____________   \\   \\" << endl;
+    cout << "     |           |     \\_____/     |                     \\_     |          |  \\   \\___\\" << endl;
+    cout << "     |           |                 |                       /--__|  ________|--/\\___/---\\" << endl;
+    cout << "     |           |                 |                      /NYC o/_/ ________ " << endl;
+    cout << "     |           |                /|                      \\   /_ /______/" << endl;
+    cout << "     |           |               | |                  PHI o\\    /" << endl;
+    cout << "     |           |              _/ |____________________/-/    /" << endl;
+    cout << "     |       __/ \\____      __/        |__/ \\__      _/|\\\\__ /" << endl;
+    cout << "     /   ___/         \\____/              ____/ \\DC o| || | //" << endl;
+    cout << "    /___/                |              _/        \\_ | ||__|" << endl;
+    cout << "    |                    \\             /            \\|  \\__/" << endl;
+}
+
 int main() {
     CityNetwork network;
-
+    cout << "Welcome to the Trip Planner!" << endl;
+    print_map();
+    cout << endl << "Routes: ";
     // addint routes
    network.addRoute("NYC", "Chicago", 200, 2, "airplane");
-network.addRoute("NYC", "Boston", 100, 4, "train");
-network.addRoute("NYC", "Washington DC", 180, 1.5, "airplane");
-network.addRoute("NYC", "Philadelphia", 50, 22, "bus");
-network.addRoute("Chicago", "NYC", 200, 2, "airplane");
-network.addRoute("Chicago", "Boston", 90, 15, "train");
-network.addRoute("Chicago", "Washington DC", 180, 1.5, "airplane");
-network.addRoute("Chicago", "Philadelphia", 80, 12, "train");
-network.addRoute("Boston", "NYC", 40, 4, "bus");
-network.addRoute("Boston", "Chicago", 70, 10, "train");
-network.addRoute("Boston", "Washington DC", 160, 1.5, "airplane");
-network.addRoute("Boston", "Philadelphia", 30, 6, "bus");
-network.addRoute("Washington DC", "NYC", 120, 3, "train");
-network.addRoute("Washington DC", "Chicago", 150, 3, "airplane");
-network.addRoute("Washington DC", "Boston", 90, 8, "train");
-network.addRoute("Washington DC", "Philadelphia", 130, 1, "airplane");
-network.addRoute("Philadelphia", "NYC", 80, 1, "train");
-network.addRoute("Philadelphia", "Chicago", 150, 2.5, "airplane");
-network.addRoute("Philadelphia", "Boston", 35, 6, "bus");
-network.addRoute("Philadelphia", "Washington DC", 80, 2, "train");
-
-
-
-    // print routes
+    network.addRoute("NYC", "Boston", 100, 4, "train");
+    network.addRoute("NYC", "Washington DC", 180, 1.5, "airplane");
+    network.addRoute("NYC", "Philadelphia", 50, 22, "bus");
+    network.addRoute("Chicago", "NYC", 200, 2, "airplane");
+    network.addRoute("Chicago", "Boston", 90, 15, "train");
+    network.addRoute("Chicago", "Washington DC", 180, 1.5, "airplane");
+    network.addRoute("Chicago", "Philadelphia", 80, 12, "train");
+    network.addRoute("Boston", "NYC", 40, 4, "bus");
+    network.addRoute("Boston", "Chicago", 70, 10, "train");
+    network.addRoute("Boston", "Washington DC", 160, 1.5, "airplane");
+    network.addRoute("Boston", "Philadelphia", 30, 6, "bus");
+    network.addRoute("Washington DC", "NYC", 120, 3, "train");
+    network.addRoute("Washington DC", "Chicago", 150, 3, "airplane");
+    network.addRoute("Washington DC", "Boston", 90, 8, "train");
+    network.addRoute("Washington DC", "Philadelphia", 130, 1, "airplane");
+    network.addRoute("Philadelphia", "NYC", 80, 1, "train");
+    network.addRoute("Philadelphia", "Chicago", 150, 2.5, "airplane");
+    network.addRoute("Philadelphia", "Boston", 35, 6, "bus");
+    network.addRoute("Philadelphia", "Washington DC", 80, 2, "train");
     network.printRoutes();
-
     return 0;
 }
